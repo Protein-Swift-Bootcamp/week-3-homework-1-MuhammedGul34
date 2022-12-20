4c45ec812e1264fabb0cb6632cf2bd144ca2d2f6
MuhammedGul34 <116461079+MuhammedGul34@users.noreply.github.com>
2022-12-20 20:11:00 +0300

    Filing Types

---
+++ HW1(Closures)/HW1(Closures)/CustomUI/SetUpCustomLabels.swift
@@ -0,0 +1,16 @@
+//
+//  SetUpCustomLabels.swift
+//  HW1(Closures)
+//
+//  Created by Muhammed Gül on 20.12.2022.
+//
+
+import UIKit
+
+class SetupCustomUILabels {
+    func setupUIView(UIview : UIView,_ cornerRadius: CGFloat){
+        UIview.layer.masksToBounds = true
+        UIview.clipsToBounds = true
+        UIview.layer.cornerRadius = cornerRadius
+    }
+}
